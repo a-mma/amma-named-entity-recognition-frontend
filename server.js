7f09c4c906fce8d2e8fae07ceb3500186696ef41
Shyam <shyam.mohan@perfomatix.com>
2018-04-10 11:22:07 +0530

    Deployment

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // server.js
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const app = express();
-const compression = require('compression');
-
-const forceSSL = function () {
-  return function (req, res, next) {
+ 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function(req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(
         ['https://', req.get('Host'), req.url].join('')
@@ -15,12 +18,25 @@ const forceSSL = function () {
     next();
   }
 };
-
-// app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
-app.use(compression());
+ 
+// Instruct the app
+// to use the forceSSL
+// middleware
+//app.use(forceSSL());
+ 
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+/*
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+*/
+ 
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080, function () {
+app.listen(process.env.PORT || 4200, function() {
   console.log('Listening on port ' + this.address().port); //Listening on port 8888
 });
